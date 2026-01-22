e72f335e3535fcc41c1e940267fcdd31bb660fd9
lakhanmeena7109-afk <lakhanmeena7109@gmail.com>
2026-01-22 17:32:43 +0530

    Project initlization

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
+
+const app=express()
+const port = process.env.PORT || 3200
+
+app.use(cors())
+
+app.get("/health",(req,res)=>{
+   res.status(200).send({
+    massage:"I am ok And Running"
+   })
+})
+
+app.listen(port,()=>{console.log(`App running on Port${port}`)})
+
+
